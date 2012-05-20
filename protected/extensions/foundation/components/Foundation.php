7c2c9fffe953c337a493ed8510d4294124fdd688
Alex Urbano <asgaroth.belem@gmail.com>
2012-05-20 15:38:50 -0500

    Javascripts are not registered at CClientScript::POS_END

--- protected/extensions/foundation/components/Foundation.php
+++ protected/extensions/foundation/components/Foundation.php
@@ -12,80 +12,79 @@
  */
 class Foundation extends CApplicationComponent {
 
-	/**
-	 * @var boolean whether to register the Foundation core CSS (foundation.css).
-	 * Defaults to true.
-	 */
-	public $coreCss = true;
-	/**
-	 * @var boolean whether to register jQuery and the Foundation JavaScript.
-	 */
-	public $enableJS = true;
+    /**
+     * @var boolean whether to register the Foundation core CSS (foundation.css).
+     * Defaults to true.
+     */
+    public $coreCss = true;
+    /**
+     * @var boolean whether to register jQuery and the Foundation JavaScript.
+     */
+    public $enableJS = true;
 
-	protected $_assetsUrl;
+    protected $_assetsUrl;
 
-	/**
-	 * Initializes the component.
-	 */
-	public function init() {
-		if (!Yii::getPathOfAlias('foundation'))
-			Yii::setPathOfAlias('foundation', realpath(dirname(__FILE__) . '/..'));
+    /**
+     * Initializes the component.
+     */
+    public function init( ) {
+        if( !Yii::getPathOfAlias( 'foundation' ) )
+            Yii::setPathOfAlias( 'foundation', realpath( dirname( __FILE__ ).'/..' ) );
 
-		if ($this -> coreCss) {
-			$this -> registerCss();
-		}
+        if( $this->coreCss ) {
+            $this->registerCss( );
+        }
 
-		if ($this -> enableJS) {
-			$this -> registerJs();
-		}
-	}
+        if( $this->enableJS ) {
+            $this->registerJs( );
+        }
+    }
 
+    /**
+     * Registers the Foundation CSS.
+     */
+    public function registerCss( ) {
+        Yii::app( )->clientScript->registerCssFile( $this->getAssetsUrl( ).'/stylesheets/foundation.css' );
+    }
 
-	/**
-	 * Registers the Bootstrap CSS.
-	 */
-	public function registerCss() {
-		Yii::app() -> clientScript -> registerCssFile($this -> getAssetsUrl() . '/stylesheets/foundation.css');
-	}
+    /**
+     * Registers the core JavaScript plugins.
+     * @since 0.9.8
+     */
+    protected function registerJs( ) {
+        Yii::app( )->clientScript->registerCoreScript( 'jquery' );
+        $this->registerScriptFile( 'modernizr.foundation.js' );
+        $this->registerScriptFile( 'foundation.js' );
+        $this->registerScriptFile( 'app.js' );
+    }
 
-	/**
-	 * Registers the core JavaScript plugins.
-	 * @since 0.9.8
-	 */
-	protected function registerJs() {
-		Yii::app() -> clientScript -> registerCoreScript('jquery');
-		Yii::app() -> clientScript -> registerScriptFile($this -> getAssetsUrl() . '/javascripts/modernizr.foundation.js');
-		Yii::app() -> clientScript -> registerScriptFile($this -> getAssetsUrl() . '/javascripts/foundation.js');
-		Yii::app() -> clientScript -> registerScriptFile($this -> getAssetsUrl() . '/javascripts/app.js');
-	}
+    /**
+     * Registers a JavaScript file in the assets folder.
+     * @param string $fileName the file name.
+     * @param integer $position the position of the JavaScript file.
+     */
+    public function registerScriptFile( $fileName, $position = CClientScript::POS_END ) {
+        Yii::app( )->clientScript->registerScriptFile( $this->getAssetsUrl( ).'/javascripts/'.$fileName, $position );
+    }
 
-	/**
-	 * Registers a JavaScript file in the assets folder.
-	 * @param string $fileName the file name.
-	 * @param integer $position the position of the JavaScript file.
-	 */
-	public function registerScriptFile($fileName, $position = CClientScript::POS_END) {
-		Yii::app() -> clientScript -> registerScriptFile($this -> getAssetsUrl() . '/javascripts/' . $fileName, $position);
-	}
-	
-	/**
-	 * Registers a CSS file in the assets folder.
-	 * @param string $fileName the file name.
-	 */
-	public function registerCssFile($fileName, $position = CClientScript::POS_END) {
-		Yii::app() -> clientScript -> registerCssFile($this -> getAssetsUrl() . '/stylesheets/' . $fileName);
-	}
+    /**
+     * Registers a CSS file in the assets folder.
+     * @param string $fileName the file name.
+     */
+    public function registerCssFile( $fileName, $position = CClientScript::POS_END ) {
+        Yii::app( )->clientScript->registerCssFile( $this->getAssetsUrl( ).'/stylesheets/'.$fileName );
+    }
 
-	/**
-	 * Returns the URL to the published assets folder.
-	 * @return string the URL
-	 */
-	protected function getAssetsUrl() {
-		if ($this -> _assetsUrl == null) {
-			$assetsPath = Yii::getPathOfAlias('foundation.lib.foundation');
-			$this -> _assetsUrl = Yii::app() -> assetManager -> publish($assetsPath, false, -1, YII_DEBUG);
-		}
-		return $this -> _assetsUrl;
-	}
+    /**
+     * Returns the URL to the published assets folder.
+     * @return string the URL
+     */
+    protected function getAssetsUrl( ) {
+        if( $this->_assetsUrl == null ) {
+            $assetsPath = Yii::getPathOfAlias( 'foundation.lib.foundation' );
+            $this->_assetsUrl = Yii::app( )->assetManager->publish( $assetsPath, false, -1, YII_DEBUG );
+        }
+        return $this->_assetsUrl;
+    }
 
 }
